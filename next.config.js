72a758a6cfdde6a03a82b6cad69f12abebf6a465
Christos Pengaras <c.pengaras@gmail.com>
2023-10-06 22:52:31 +0300

    Changes on next.congif.js file

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'export',
     images: {
         domains: ["cdn.imagin.studio"]
-    }
-}
+    },
+ }
 
 module.exports = nextConfig
+
