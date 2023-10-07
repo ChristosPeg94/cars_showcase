5dce950623022df3a7c649d6567f9897b16ece8e
Christos Pengaras <c.pengaras@gmail.com>
2023-10-07 17:37:38 +0300

    Corrections to the code

--- next.config.js
+++ next.config.js
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
     images: {
         domains: ["cdn.imagin.studio"]
-    },
- }
+    }
+}
 
 module.exports = nextConfig
-
